2414d8ee07f6481db0c6568a87a21cba9f7dadbc
Siqueira's <39175488+Natan0202@users.noreply.github.com>
2022-08-13 23:38:55 -0300

    2.0v
    Adicionando páginas com estilização e banco de dados

--- estagio/incluidoComum/verificar.php
+++ estagio/incluidoComum/verificar.php
@@ -16,7 +16,8 @@
     $exe = mysqli_query($conne, $sql);
     $resultado = mysqli_num_rows($exe);
 
-
+    /*Verificação se o CPF é o mesmo de remetente*/
+    //Anexo 02
     $ver = "SELECT cpf FROM comum WHERE cpf = '{$destino}'";
     $verificar = mysqli_query($conne, $ver);
     $verificado = mysqli_num_rows($verificar);
@@ -26,6 +27,8 @@
 
     $exec = $conne->query($consulta);
     
+    //Anexo 03
+    /*Capturando o valor de um campo no SQL para colocar em uma variável*/
     while($row = $exec->fetch_assoc()){
         $var = $row['valor'];
     }
@@ -33,9 +36,10 @@
 
     /*Pegando valor da tabela do usuário cujo o CNPJ é fornecido*/
 
-    $consulta = "SELECT valor FROM lojista WHERE cnpj = '{$destino}'";
+    //Anexo 04
+    $consulta2 = "SELECT valor FROM lojista WHERE cnpj = '{$destino}'";
 
-    $executar = $conne->query($consulta);
+    $executar = $conne->query($consulta2);
     $var2 = 0;
     while($row = $executar->fetch_assoc()){
         $var2 = $row['valor'];
@@ -44,15 +48,21 @@
 
     /**/
 
+    /*Verificando se o CPF/CNPJ é válido e se o valor de saldo é maior que 0*/
     if($resultado){
         echo "<br>Transferência não disponível por questão de Saldo! Ou CPF/CNPJ inválido";
     }
+
+    /*No 1º While é capturado o valor do campo no SQL (valor) e aqui faço um calculo para saber
+    se o valor que vai se transferido é maior do que o saldo*/
     elseif($cal < 0){
         echo "Valor a ser tranferido é menor do que seu saldo!";
     }
+
+    /*Aqui verifica se o CPF que vai ser o destino é o mesmo do remetente*/
     elseif($verificado){
         echo "CPF não pode ser do destinatário! ";
-    }
+    }/*Aqui está executando a transferência*/
     else{
         $command = "UPDATE lojista SET valor = $soma WHERE cnpj = '{$destino}'";
         $executando = mysqli_query($conne, $command);
--- estagio/incluindoLoja/incluir.php
+++ estagio/incluindoLoja/incluir.php
@@ -5,28 +5,35 @@
     if(empty($_POST['cnpj']) || empty($_POST['email'])){
         header("Location: usuarios.php");
     }
+
+    //Capturando valores via forms e sessão
     $nome = mysqli_real_escape_string($conne, $_POST['nome']);
     $cnpj = mysqli_real_escape_string($conne, $_POST['cnpj']);
     $email = mysqli_real_escape_string($conne, $_POST['email']);
     $senha = mysqli_real_escape_string($conne, $_POST['senha']);
 
+    //fazendo consulta
     $sql = "SELECT cnpj FROM lojista WHERE cnpj = '{$cnpj}' ";
 
     $executar = mysqli_query($conne, $sql);
 
     $resultado = mysqli_num_rows($executar);
 
+
+    //caso exista redireciona o usuário
     if($resultado){
         $_SESSION['existe'] = $cnpj;
         header("Location: usuarios.php");
     }
+
+    //inserido lojista ou usuário comum
     else{
         $comando = "INSERT INTO lojista (nome, cnpj,email,senha) VALUES ('{$nome}', '{$cnpj}','{$email}','{$senha}')";
         $update = "ALTER TABLE lojista ADD COLUMN valor int;";
         $exe = mysqli_query($conne, $comando);
         $exe = mysqli_query($conne, $update);
         $_SESSION['criado'] = $nome;
-        header("Location: transferir.php");
+        header("Location: lojistas.php");
     }
 
 
